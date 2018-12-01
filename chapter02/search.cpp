334be47817e34d7ce6c1e23a594446206475fd1d
dondakeshimo <went.went.takkun135@gmail.com>
2018-12-01 23:28:58 +0900

    [add]add better linear search

--- chapter02/search.cpp
+++ chapter02/search.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 
 int Linear_Search(string* A, int n, string x);
+int Better_Linear_Search(string* A, int n, string x);
 
 
 int main()
@@ -13,7 +14,7 @@ int main()
     string A[n] = {"Alex", "Bob", "Chris"};
     string x = "Bob";
 
-    return Linear_Search(A, n, x);
+    return Better_Linear_Search(A, n, x);
 }
 
 
@@ -28,4 +29,21 @@ int Linear_Search(string* A, int n, string x)
     cout <<  answer << endl;
 
     return answer;
+}
+
+
+int Better_Linear_Search(string* A, int n, string x)
+{
+    int answer = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (x == A[i]) {
+            cout << "answer = " <<  i << endl;
+            return i;
+        }
+    }
+
+    cout <<  "answer = " << -1 << endl;
+
+    return -1;
 }
