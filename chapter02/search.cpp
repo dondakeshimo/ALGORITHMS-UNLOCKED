0760b28026bbb48a23255e400d01dc644d7e25f3
dondakeshimo <went.went.takkun135@gmail.com>
2018-12-01 23:49:55 +0900

    [add]add recursive binary search

--- chapter02/search.cpp
+++ chapter02/search.cpp
@@ -7,15 +7,16 @@ using namespace std;
 int Linear_Search(string* A, int n, string x);
 int Better_Linear_Search(string* A, int n, string x);
 int Binary_Search(string* A, int n, string x);
+int Recursive_Binary_Search(string* A, int p, int r, string x);
 
 
 int main()
 {
     int n = 5;
     string A[n] = {"Alex", "Bob", "Chris", "Dave", "Eathern"};
-    string x = "Dave";
+    string x = "Alex";
 
-    cout << "answer = " << Binary_Search(A, n, x) << endl;
+    cout << "answer = " << Recursive_Binary_Search(A, 0, 4, x) << endl;
     return 0;
 }
 
@@ -62,4 +63,20 @@ int Binary_Search(string* A, int n, string x)
     }
 
     return -1;
+}
+
+
+int Recursive_Binary_Search(string* A, int p, int r, string x)
+{
+    int q;
+
+    if (p > r) return -1;
+    else {
+        q = (p + r) / 2;
+        if (A[q] == x) return q;
+        else {
+            if (A[q] > x) return Recursive_Binary_Search(A, p, q - 1, x);
+            else return Recursive_Binary_Search(A, q + 1, r, x);
+        }
+    }
 }
