00a838a0003ce754fd68cf7849b64140c2714280
dondakeshimo <went.went.takkun135@gmail.com>
2018-12-04 23:39:46 +0900

    [add]add Insertion_Sort

--- sort.cpp
+++ sort.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 
 string* Selection_Sort(string* A, int n);
+string* Insertion_Sort(string* A, int n);
 
 
 int main()
@@ -14,7 +15,7 @@ int main()
     string x = "Alex";
     string* answer;
 
-    answer = Selection_Sort(A, n);
+    answer = Insertion_Sort(A, n);
     for (int i = 0; i < n; i++) {
         cout << answer[i] << endl;
     }
@@ -37,6 +38,27 @@ string* Selection_Sort(string* A, int n)
         temp = A[i];
         A[i] = A[smallest];
         A[smallest] = temp;
+    }
+
+    return A;
+}
+
+
+string* Insertion_Sort(string* A, int n)
+{
+    int j;
+    string key;
+
+    for (int i = 1; i < n; i++) {
+        key = A[i];
+        j = i - 1;
+
+        while (j > 0 && A[j] > key) {
+            A[j + 1] = A[j];
+            j--;
+        }
+
+        A[j + 1] = key;
     }
 
     return A;
