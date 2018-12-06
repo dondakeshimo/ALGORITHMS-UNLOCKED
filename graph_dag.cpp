55667e48f572aa18fff3ce7766f137548b0cb4c8
dondakeshimo <went.went.takkun135@gmail.com>
2018-12-06 22:27:04 +0900

    [fix]remove unneccesary n

--- graph_dag.cpp
+++ graph_dag.cpp
@@ -7,10 +7,11 @@
 using namespace std;
 
 
-vector<int> Topological_Sort(vector<vector<int>>& G, int n);
-void Rerax(vector<int>& shortest, vector<int>& pred,
+vector<int> Topological_Sort(vector<vector<int>>& G);
+void Relax(vector<int>& shortest, vector<int>& pred,
            vector<vector<int>>& weight, int u, int v);
-vector<int> Dag_Shortest_Paths(vector<vector<int>>& G, int n, int s);
+vector<int> Dag_Shortest_Paths(vector<vector<int>>& G,
+                               vector<vector<int>>& weight, int n, int s);
 
 
 int main()
@@ -38,26 +39,26 @@ int main()
     cout << "Topological sort" << endl;
     cout << "-------------" << endl;
 
-    answer = Topological_Sort(G, n);
+    answer = Topological_Sort(G);
     for (int i = 0; i < n; i++) {
         cout << answer[i] << endl;
     }
 }
 
 
-vector<int> Topological_Sort(vector<vector<int>>& G, int n)
+vector<int> Topological_Sort(vector<vector<int>>& G)
 {
-    vector<int> in_degree(n, 0);
+    vector<int> in_degree(G.size(), 0);
     vector<int> next;
     vector<int> linear_order;
 
-    for (int u = 0; u < n; u++) {
-        for (int v = 0; v < n; v++) {
+    for (int u = 0; u < G.size(); u++) {
+        for (int v = 0; v < G.size(); v++) {
             if (G[u][v] == 1) in_degree[v]++;
         }
     }
 
-    for (int u = 0; u < n; u++) {
+    for (int u = 0; u < G.size(); u++) {
         if (in_degree[u] == 0) next.push_back(u);
     }
 
@@ -67,7 +68,7 @@ vector<int> Topological_Sort(vector<vector<int>>& G, int n)
         next.pop_back();
         linear_order.push_back(u);
 
-        for (int v = 0; v < n; v++) {
+        for (int v = 0; v < G.size(); v++) {
             if (G[u][v] == 1) {
                 in_degree[v]--;
                 if (in_degree[v] == 0) next.push_back(v);
@@ -76,7 +77,7 @@ vector<int> Topological_Sort(vector<vector<int>>& G, int n)
     }
 
     // this process is to linear order same as book number
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < G.size(); i++) {
         linear_order[i]++;
     }
 
@@ -84,21 +85,22 @@ vector<int> Topological_Sort(vector<vector<int>>& G, int n)
 }
 
 
-void Rerax(vector<int>& shortest, vector<int>& pred,
+void Relax(vector<int>& shortest, vector<int>& pred,
            vector<vector<int>>& weight, int u, int v)
 {
-    if (shortest[u] + weithg[u][v] < shortest[v]) {
+    if (shortest[u] + weight[u][v] < shortest[v]) {
         shortest[v] = shortest[u] + weight[u][v];
         pred[v] = u;
     }
 }
 
 
-vector<int> Dag_Shortest_Paths(vector<vector<int>>& G, int n, int s)
+vector<int> Dag_Shortest_Paths(vector<vector<int>>& G,
+                               vector<vector<int>>& weight, int n, int s)
 {
-    vector<int> l = Topological_Sort(G, n);
+    vector<int> l = Topological_Sort(G);
     vector<int> shortest(n, numeric_limits<int>::max());
-    vector<int> pred(n, NULL);
+    vector<int> pred(n, numeric_limits<int>::max());
 
     shortest[s] = 0;
 
